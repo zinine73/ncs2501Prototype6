32fce84c823fe1b98fbea0e9bea3e6f27a203801
zinine73 <zinine@naver.com>
2025-03-28 15:53:05 +0900

    Json

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    public Color TeamColor;
+    private void Awake()
+    {
+        TeamColor = Color.red;
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = this.TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        try
+        {
+            File.WriteAllText(Application.persistentDataPath
+                + "/savefile.json", json);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log($"Failed to save Json : {ex}");
+        }
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath
+            + "/savefile.json";
+        try
+        {
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+                this.TeamColor = data.TeamColor;
+            }
+            else
+            {
+                Debug.Log("File not exist");
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log($"Faile to load Json : {ex}");
+        }
+    }
+}
--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,10 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+    
+    public void BackToMain()
+    {
+        //SceneManager.LoadScene(0);
+        SceneManager.LoadScene("Menu");
+    }
 }
